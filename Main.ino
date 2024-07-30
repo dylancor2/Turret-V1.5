aa25983f0811b3695811a5bac65ded5b0a8126d6
dylancor2 <137826466+dylancor2@users.noreply.github.com>
2024-07-30 18:18:12 -0400

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -16,22 +16,56 @@ uint8_t rowPins[ROWS] = { 26, 22, 21, 20 }; // Pins connected to R1, R2, R3, R4
 uint8_t colPins[COLS] = { 19, 18, 17, 16}; // Pins connected to C1, C2, C3, C4
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+bool passSet = false;
+int passPos = 0;
+String password[10];
+
 
 void setup() {
-  Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.print("Enter Password: ");
-  lcd.setCursor(0, 1);
+  start();
+
 }
 
 void loop() {
   char key = keypad.getKey();
 
   if (key != NO_KEY) {
-    lcd.print(key);
-    Serial.println(key);
-    delay(200);
+    if(passSet == false){
+      pass(key);
+    }
   }
-  delay(20);
-  key = NO_KEY;
+  delay(10);
+}
+
+void start(){
+  lcd.setCursor(0, 0);
+  slowprint("Welcome to Turrt");
+  lcd.setCursor(0, 1);
+  slowprint("~~~~~~~~~~~~~~~~");
+  lcd.clear();
+  slowprint(" Enter New Pass ");
+  lcd.setCursor(0, 1);
+  delay(100);
+  lcd.print("  (          )  ");
+}
+
+void slowprint(String mes) {
+  for(int i = 0;i < mes.length();i++){
+    lcd.print(mes[i]);
+    delay(100);
+  }
+}
+
+void pass(char next){
+  lcd.setCursor(3, 1);
+  password[passPos] = next;
+  passPos++;
+  for(int i = 0; i < passPos; i++){
+    lcd.print(password[i]);
+  }
+  for(int i = 0; i < 10-passPos; i++){
+    lcd.print(" ");
+  }
+  lcd.print(")  ");
 }
