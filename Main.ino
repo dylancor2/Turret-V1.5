7f20d86006ae41bea7a91cca8e470249dbd22780
dylancor2 <137826466+dylancor2@users.noreply.github.com>
2024-07-30 13:03:59 -0400

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,4 +1,7 @@
 #include <Keypad.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
 
 const uint8_t ROWS = 4;
 const uint8_t COLS = 4;
@@ -9,25 +12,26 @@ char keys[ROWS][COLS] = {
   { '*', '0', '#', 'D' }
 };
 
-uint8_t colPins[COLS] = { 20, 19, 18, 17}; // Pins connected to C1, C2, C3, C4
-uint8_t rowPins[ROWS] = { 27, 26, 22, 21 }; // Pins connected to R1, R2, R3, R4
+uint8_t rowPins[ROWS] = { 26, 22, 21, 20 }; // Pins connected to R1, R2, R3, R4
+uint8_t colPins[COLS] = { 19, 18, 17, 16}; // Pins connected to C1, C2, C3, C4
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-  pinMode(15, OUTPUT);
-  Serial.begin(115200);
-  digitalWrite(15, LOW);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Enter Password: ");
+  lcd.setCursor(0, 1);
 }
 
 void loop() {
   char key = keypad.getKey();
 
   if (key != NO_KEY) {
-    digitalWrite(15, LOW);
-    delay(1000);
-    digitalWrite(15, HIGH);
-    delay(1000);
+    lcd.print(key);
     Serial.println(key);
+    delay(200);
   }
+  delay(20);
+  key = NO_KEY;
 }
